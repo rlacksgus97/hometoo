f12a07a68b48e01a9eae81ad8fa779964265e35d
JHS <jinhs1995@gmail.com>
2021-10-19 23:10:34 +0900

    feat: react 템플릿 내재화
    1) src 하위 index.js를 통해 routing (controller역할)
    2) src/views 안에 있는 것들을 렌더링

---
+++ front/src/index.js
@@ -0,0 +1,67 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
+import Home from "views/Home.js";
+import Project from "views/Project.js";
+import Sample from "./views/Sample";
+import Landing from "./views/Landing.js";
+import Login from "./views/Login.js";
+import Profile from "./views/Profile";
+import Register from "./views/Register";
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route
+                path="/"
+                exact
+                render={props => <Home {...props} />}
+            />
+            <Route
+                path="/home"
+                exact
+                render={props => <Home {...props} />}
+            />
+            <Route
+                path="/project"
+                exact
+                render={props => <Project {...props} />}
+            />
+            <Route
+                path="/sample"
+                exact
+                render={props => <Sample {...props} />}
+            />
+            <Route
+                path="/landing"
+                exact
+                render={props => <Landing {...props} />}
+            />
+            <Route
+                path="/login"
+                exact
+                render={props => <Login {...props} />}
+            />
+            <Route
+                path="/profile"
+                exact
+                render={props => <Profile {...props} />}
+            />
+            <Route
+                path="/register"
+                exact
+                render={props => <Register {...props} />}
+            />
+            <Redirect to="/" />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
